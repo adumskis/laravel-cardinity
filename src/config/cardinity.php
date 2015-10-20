828aecc287da3ab764da61413ed961bfd596df06
Algirdas <algirdasdum@gmail.com>
2015-10-20 16:05:31 +0300

    First commmit

---
+++ src/config/cardinity.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'customer_key' => 'test_oiceouuqvm01bpuuztbqv5zc933aon',
+    'customer_secret' => 'vxjptkhv81aahi9ye4iqhlveppjwnnqrysm6uzrz34iubgkhbx',
+
+    'currency' => 'EUR',
+    'country' => 'LT',
+    'settle' => false,
+
+    'statuses' => [
+        'pending' => 'payment_pending',
+        'approved' => 'payment_approved',
+        'declined' => 'payment_declined'
+    ]
+
+];
